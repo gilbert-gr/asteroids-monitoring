2af97ac6fbbaa773f0fba778d651108187075190
Gilbert <sgil.grange@gmail.com>
2026-07-22 11:59:29 +0200

    Improve error handling

--- frontend/src/app/components/App.jsx
+++ frontend/src/app/components/App.jsx
@@ -1,22 +1,36 @@
 import Dashboard from "./Dashboard";
+import Title from "./Title";
 
 async function App() {
     let data;
     try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/monitoring`);
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/monitoring`,
+    {
+        next: {
+            revalidate: 3600,
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error("Unable to retrieve asteroid data.");
+        }
+
     data = await response.json();
+
     } catch (err) {
     data = { message: `There was an error: ${err}` };
-    }
-
-    if (data.error) {
         return (
-            <div>
-            <Title />
-            <p>{data.error}</p>
+            <div className="error-container">
+                <Title />
+                <div className="card">
+                    <h2>🚨 Something went wrong</h2>
+                    <p>Unable to load asteroid data. Please try again later.</p>
+                </div>
             </div>
         );
         }
+
     
     return (
         <Dashboard data={data} />
